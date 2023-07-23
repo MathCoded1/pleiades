cf494f39f405ac54acdb1ca95c7189756c28b16e
Jonathan Paul Wolfe <=>
2023-07-22 23:45:35 -0600

    fixed

--- src/datapoint.rs
+++ src/datapoint.rs
@@ -1,18 +1,12 @@
 // Define a simple DataPoint struct
 #[derive(Clone, Debug)]
 pub struct DataPoint {
-    x: f64,
-    y: f64,
+    pub x: f64,
+    pub y: f64,
 }
 impl DataPoint{
     pub fn new(x:f64, y:f64) ->Self{
         DataPoint{x,y}
     }
-    pub fn get_x(&self) ->f64{
-        self.x
-}
-    pub fn get_y(&self) ->f64{
-    self.y
-}
 
 }
